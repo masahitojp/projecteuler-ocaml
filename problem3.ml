4e5fe29cc1a894ca8ce293a1170e601b6d36ddf0
masahitojp <randomstep@gmail.com>
2014-03-16 01:44:56 +0900

    fixed problem3 code

--- problem3.ml
+++ problem3.ml
@@ -1,10 +1,15 @@
-
 let rec loop n p =
-  if (n < p * p) then [n]
-  else if (n mod p = 0) then p :: loop (n / p) p
-  else loop n (p + 1) in 
+  match n with
+    n when (n < p * p) ->
+    [n]
+  | n when (n mod p = 0) ->
+     p :: loop (n / p) p
+  | _ ->
+    loop n (p + 1)
+    in 
 
     let factor n =
-      loop n 2 in 
+      loop n 2
+    in 
 
     print_int (List.hd (List.rev (factor 600851475143)));;
